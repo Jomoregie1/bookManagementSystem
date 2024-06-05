21fd934d0af602d688ca4b7be5d10a0f8334ddcd
Joseph <josephomoregie1@outlook.com>
2024-06-05 12:53:43 +0100

    add implementation to delete Order

--- src/main/java/com/bookstore/managementsystem/service/OrderServiceImpl.java
+++ src/main/java/com/bookstore/managementsystem/service/OrderServiceImpl.java
@@ -75,6 +75,8 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public ResponseEntity<Void> deleteOrder(Long id) {
-        return null;
+        boolean exists = orderRepo.existsById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
