7d0fda5b89fa822923a9bf974017eddc1fc1dae7
Joseph <josephomoregie1@outlook.com>
2024-06-06 17:25:51 +0100

    changed made to orderDto

--- src/main/java/com/bookstore/managementsystem/dto/OrderDto.java
+++ src/main/java/com/bookstore/managementsystem/dto/OrderDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @Builder
@@ -15,4 +16,5 @@ public class OrderDto {
 
     private LocalDate date;
     private double totalAmount;
+    private Set<String> bookTitles;
 }
